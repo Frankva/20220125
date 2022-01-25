8142390c4cd117a5126ce449fbf144cbd37c4553
Marc <marclouis.porta@formation.orif.ch>
2022-01-25 14:49:39 +0100

    read

---
+++ read.py
@@ -0,0 +1,31 @@
+import sys
+from time import sleep
+import RPi.GPIO
+from MFRC522 import SimpleMFRC522 as Reader
+
+class App:
+    def __init__(self) -> None:
+        self.debug = True
+        if self.debug:
+            print("App.init", file=sys.stderr)
+        self.running = True
+        self.reader = Reader()
+
+    def __del__(self):
+        if self.debug:
+            print("App.del", file=sys.stderr)
+        RPi.GPIO.cleanup()
+    def main(self):
+        while self.running:
+            self.update()
+    def update(self):
+        id, text = self.reader.read()
+        print(f"id : {id}, text : {text}")
+        sleep(5)
+
+def main():
+    app = App()
+    app.main()
+
+if __name__ == "__main__":
+    main()
