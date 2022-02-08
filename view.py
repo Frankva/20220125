40d3e95f21390c319f526b65f8dd6d107c3f4ee6
Marc <marclouis.porta@formation.orif.ch>
2022-02-08 13:56:57 +0100

    add buttons

--- view.py
+++ view.py
@@ -3,27 +3,36 @@ import pygame
 class Button:
     def __init__(self):
         x, y = view.screen.get_size()
-        self.x = x / 2
-        self.y = y / 2
-        self.w = x / 3
-        self.h = y / 3
+        self.x = 7 * (x / 12)
+        self.y = 1* (y / 12)
+        self.w = 4 * (x / 12)
+        self.h = 10 * (y / 12)
         
         self.color = pygame.Color("blue")
         self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
     def set_red_pos(self):
-        pass
+        print("set_red_pos")
+        x, y = view.screen.get_size()
+        self.color = pygame.Color("red")
+        self.x = x / 12
+        self.y = y / 12
+        self.w = 4 * (x / 12)
+        self.h = 10 * (y / 12)
+        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
 
 
 class View:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 400), pygame.FULLSCREEN)#pygame.FULLSCREEN
+        self.screen = pygame.display.set_mode((800, 400))#pygame.FULLSCREEN
         self.running = True
         self.buttons = list()
         
 
     def load(self):
         self.buttons.append(Button())
+        self.buttons.append(Button())
+        self.buttons[0].set_red_pos()
         pygame.display.set_caption("test")
         while self.running:
             for event in pygame.event.get():
