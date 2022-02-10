2b02283e1a134b60634530f81b1b8b6482639e8a
Marc <marclouis.porta@formation.orif.ch>
2022-02-10 15:04:22 +0100

    corr thread

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+import os
 import threading
-import view
-import rfid
+import view 
+
+import os
 
 class App:
     '''
     controle view script, rfid script, model script
     '''
+    rasp = os.name != "nt"
+    if rasp:
+        import rfid
+        print("raspberry")
+
     def __init__(self):
-        self.view = view.View()
-        self.theard_view = threading.Thread(target=self.view.load)
-        self.rfid = rfid.Rfid()
-        self.id = list()
-        self.theard_rfid = threading.Thread(target=self.rfid.read_list, args=(self.id))
+            
+        self.view = view.View
+        self.theard_view = threading.Thread(target=self.view)
+        if self.rasp:
+            self.rfid = rfid.Rfid()
+            self.id = list()
+            self.theard_rfid = threading.Thread(target=self.rfid.read_list, args=(self.id))
 
     def load(self):
         self.theard_view.start()
-        self.theard_rfid.start()
-        self.theard_rfid.join()
-        self.view.current_scene["select"]
+        if self.rasp:
+            self.theard_rfid.start()
+            self.theard_rfid.join()
+        
+        input()
+        self.view.current_scene = "select"
 
 
 def main():
--- view.py
+++ view.py
@@ -1,8 +1,13 @@
+from distutils.debug import DEBUG
 import pygame
+import sys
 
 class Button:
-    def __init__(self):
-        x, y = view.screen.get_size()
+    def __init__(self, screen):
+        if View.debug:
+            print("Button.init", file=sys.stderr)
+        self.screen = screen
+        x, y = self.screen.get_size()
         self.x = 7 * (x / 12)
         self.y = 1* (y / 12)
         self.w = 4 * (x / 12)
@@ -10,31 +15,36 @@ class Button:
         
         self.color = pygame.Color("blue")
         self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+
     def set_red_pos(self):
-        print("set_red_pos")
-        x, y = view.screen.get_size()
+        if View.debug:
+            print("Button.set_red_pos", file=sys.stderr)
+        x, y = self.screen.get_size()
         self.color = pygame.Color("red")
         self.x = x / 12
         self.y = y / 12
         self.w = 4 * (x / 12)
         self.h = 10 * (y / 12)
         self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+        
 
 class Scene:
-    def __init__(self):
-        print("init Scene")
+    def __init__(self, screen):
+        if View.debug:
+            print("init Scene", file=sys.stderr)
         self.font = pygame.font.SysFont(None, 24)
+        self.screen = screen
 
 class SceneSelect(Scene):
-    def __init__(self):
-        
-        super().__init__()
+    def __init__(self, screen):
+        if View.debug:
+            print("SceneSelect load", file=sys.stderr)
+        super().__init__(screen)
         self.buttons = list()
-        print("SceneSelect load")
-        self.buttons.append(Button())
-        self.buttons.append(Button())
+        self.buttons.append(Button(self.screen))
+        self.buttons.append(Button(self.screen))
         self.buttons[1].set_red_pos()
-   
+        
 
 
     def draw(self, screen):
@@ -43,9 +53,9 @@ class SceneSelect(Scene):
             pygame.draw.rect(screen, button.color, button.rect)
             
 class SceneWait(Scene):
-    def __init__(self):
+    def __init__(self, screen):
         
-        super().__init__()
+        super().__init__(screen)
         self.texts = list()
         self.texts.append(self.font.render("Attente badge RFID", True, pygame.Color("white")))
 
@@ -54,20 +64,28 @@ class SceneWait(Scene):
         screen.blit(self.texts[0], (0,0))
 
 class View:
+    debug = False
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 400))#pygame.FULLSCREEN
+        pygame.display.set_caption("test")
         self.running = True
         
         self.scenes = dict()
         self._current_scene = "wait"
 
+        self.load()
+
     def load(self):
-        self.scenes["wait"] = SceneWait()
-        self.scenes["select"] = SceneSelect()
-       
-        pygame.display.set_caption("test")
+        '''
+        start pygame loop
+        '''
+        self.scenes["wait"] = SceneWait(self.screen)
+        self.scenes["select"] = SceneSelect(self.screen)
+
         while self.running:
+            if View.debug:
+                print("loop", file=sys.stderr)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -77,10 +95,16 @@ class View:
 
     def __del__(self):
         pygame.quit()
+        sys.exit()
 
     def update(self):
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
             self.running = False
+        if pygame.key.get_pressed()[pygame.K_j]:
+            if self.current_scene != "select":
+                self.current_scene = "select"
+            else:
+                self.current_scene = "wait"
 
     def draw(self):
         self.screen.fill(pygame.Color("black"))
@@ -93,12 +117,14 @@ class View:
 
     @current_scene.setter
     def current_scene(self, newScene):
+       
         if newScene in self.scenes.keys():
-            self.current_scene = newScene
+            self._current_scene = newScene
         else:
-            raise ValueError("scene in arg doos not exist")
+            print(self.scenes.keys(), file=sys.stderr)
+            raise ValueError(f"scene {newScene} does not exist")
 
     
 if __name__ == "__main__":
     view = View()
-    view.load()
+    #view.load()
